ef43cca09dd61009f3c15551dbd2e0cbbcfacf0a
unknown <ahmadabdulkadir245@gmail.com>
2023-01-14 12:42:08 -0800

    sidebar font size update

--- components/SideBarLinks.js
+++ components/SideBarLinks.js
@@ -4,7 +4,7 @@ import SideBarLink from './SideBarLink'
 
 function SideBarLinks() {
   return (
-    <div className='py-1 px-[10px] text-gray-500 capitalize'>
+    <div className='py-1 px-[10px] text-gray-500 capitalize text-md'>
         <div className='flex justify-between '>
             <button className='rounded-md border-2 border-gray-400 py-2 w-[47%]  '>Login</button>
             <button className='rounded-md border-2 border-gray-400 py-2 w-[47%]  '>Sign Up</button>
@@ -12,10 +12,10 @@ function SideBarLinks() {
         <hr className='h-[1px] w-full mt-4 bg-gray-500 '/>
 
         {/* links Area */}
-        <div className='text-lg'>
+        <div className=''>
         <div className=' flex items-center space-x-3'>
         <RiUser3Line className='w-6 h-6 ' />
-            <p className='py-5 '>my account</p>
+            <p className='py-2 '>my account</p>
         </div>
         </div>
         <SideBarLink Icon={RiUser3Line} title={'orders'}/>
